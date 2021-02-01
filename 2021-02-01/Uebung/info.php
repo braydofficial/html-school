09364aa8df049980efaa93fa65f9bdb4876dfb88
thisdudeisvegan <ramorak@canreal.net>
2021-02-01 08:38:17 +0100

    :sparkles: Added school excercise no 1
    I added the school excercise no. 1 from 2021-02-01

---
+++ 2021-02-01/Uebung/info.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <?php
+      $vorname = 'Max';
+      $nachname = 'Meier';
+      echo "Vorname: $vorname <br>";
+      echo "Nachname: $nachname";
+     ?>
+  </body>
+</html>
